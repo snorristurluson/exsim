8c01b42ad91c63d31245352a9fc78d7267e66572
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2017-11-01 19:18:08 +0000

    Added a simple authentication placeholder.

--- apps/solarsystem/lib/listener.ex
+++ apps/solarsystem/lib/listener.ex
@@ -1,23 +1,45 @@
 require Logger
 
+:user
+:token
+
 defmodule Listener do
   @moduledoc false
   def accept(port) do
     {:ok, socket} = :gen_tcp.listen(port,
                       [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Accepting connections on port #{port}"
-    loop_acceptor(socket, 1)
+    loop_acceptor(socket)
   end
 
-  defp loop_acceptor(socket, id) do
+  defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
     Logger.info "Connection established"
-    {:ok, player} = Player.start_link(Integer.to_string(id), client)
+    {:ok, pid} = Task.Supervisor.start_child(
+        Solarsystem.TaskSupervisor,
+        fn -> authentication_loop(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  def authentication_loop(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    Logger.info data
+    json = Poison.decode(data, keys: :atoms!)
+    case json do
+      {:ok, %{} = details} -> handle_login(socket, details)
+      _ ->
+        Logger.info "Invalid login"
+        authentication_loop(socket)
+    end
+  end
+
+  defp handle_login(socket, %{:user => user}) do
+    {:ok, player} = Player.start_link(user, socket)
     {:ok, pid} = Task.Supervisor.start_child(
       Solarsystem.TaskSupervisor,
-      fn -> serve(client, player) end)
-    :ok = :gen_tcp.controlling_process(client, pid)
-    loop_acceptor(socket, id + 1)
+      fn -> serve(socket, player) end)
+    :ok = :gen_tcp.controlling_process(socket, pid)
   end
 
   defp serve(socket, player) do
